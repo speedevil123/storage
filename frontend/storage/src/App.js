81d7b7ac42813addbdb4db29fd4e869acf89b773
speedevil123 <syutkin_04@mail.ru>
2024-11-30 23:50:09 +0700

    removing default react code

--- frontend/storage/src/App.js
+++ frontend/storage/src/App.js
@@ -5,18 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>edit</p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React ddd
-        </a>
+        <h1>Home page</h1>
       </header>
     </div>
   );
