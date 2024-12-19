4c4e8030963b74f01992fbed60e96a9dbf858fb7
СюткинНЮ <@PROG-2>
2024-12-19 12:02:27 +0700

    DepartmentsController implementation

--- backend/Storage/Storage.API/Controllers/DepartmentsController.cs
+++ backend/Storage/Storage.API/Controllers/DepartmentsController.cs
@@ -1,12 +1,64 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Storage.API.Contracts;
+using Storage.Core.Abstractions;
+using Storage.Core.Models;
+using System.Runtime.InteropServices;
 
 namespace Storage.API.Controllers
 {
-    public class DepartmentsController : Controller
+    [ApiController]
+    [Route("[controller]")]
+    public class DepartmentsController : ControllerBase
     {
-        public IActionResult Index()
+        private readonly IDepartmentsService _departmentsService;
+
+        public DepartmentsController(IDepartmentsService departmentService)
         {
-            return View();
+            _departmentsService = departmentService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<DepartmentsResponse>>> GetDepartments()
+        {
+            var departments = await _departmentsService.GetAllDepartments();
+
+            var response = departments.Select(d => new DepartmentsResponse(
+                d.Id,
+                d.Name,
+                d.PhoneNumber,
+                d.Email,
+                d.Address));
+
+            return Ok(response);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateDepartments([FromBody] DepartmentsRequest request)
+        {
+            var department = new Department(
+                Guid.NewGuid(),
+                request.Name,
+                request.phoneNumber,
+                request.Email,
+                request.Address);
+
+            var departmentId = await _departmentsService.CreateDepartment(department);
+            return Ok(departmentId);
+        }
+
+        [HttpPut("{id:guid}")]
+        public async Task<ActionResult<Guid>> UpdateDepartment(Guid id, [FromBody] DepartmentsRequest request)
+        {
+            var departmentId = await _departmentsService
+                .UpdateDepartment(request.Id, request.Name, request.phoneNumber, request.Email, request.Address);
+            return Ok(departmentId);
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<ActionResult<Guid>> DeleteDepartment(Guid id)
+        {
+            var departmentId = await _departmentsService.DeleteDepartment(id);
+            return Ok(departmentId);
         }
     }
 }
